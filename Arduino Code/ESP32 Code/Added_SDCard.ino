5966ee3a4656c46764951fadc5a8869fa6d09baa
LeonLongZeyu <62172956+LeonLongZeyu@users.noreply.github.com>
2024-04-16 14:46:03 -0400

    Update Added_SDCard.ino

--- Arduino Code/ESP32 Code/Added_SDCard.ino
+++ Arduino Code/ESP32 Code/Added_SDCard.ino
@@ -29,8 +29,13 @@ const float scale = 0.330;
 
 const int sd_cs = 5; //Thing Plus C
 
-// data.txt file
+// dataFile.csv file
 File dataFile;
+
+// Global variables to manage timing
+unsigned long previousMillis = 0;
+const long intervalStationary = 30000; // Docked
+const long intervalMoving = 5000; // Moving
 
 void setup() {
     Serial.begin(115200);
@@ -100,12 +105,14 @@ void setup() {
     }
     Serial.println("Card Mount Success");
 
+    // Initialize directory in SD Card
     if (!SD.mkdir("/logs")) {
       Serial.println("Directory creation failed");
     } else {
       Serial.println("Directory created");
     }
 
+    // Setup Complete
     Serial.println("Initializations done...");
     delay(1000);
 }
@@ -149,162 +156,153 @@ void loop() {
     Serial.print(zAccel, 3);
     Serial.println(" g");
 
-    static bool gpsDataProcessed = false;
-    gpsDataProcessed = false;
+      static bool gpsDataProcessed = false;
+      gpsDataProcessed = false;
 
-    // Read and print SEN5x measurements
-    float massConcentrationPm1p0, massConcentrationPm2p5, massConcentrationPm4p0, massConcentrationPm10p0;
-    float ambientHumidity, ambientTemperature, vocIndex, noxIndex;
-    uint16_t sen5xError = sen5x.readMeasuredValues(
-        massConcentrationPm1p0, massConcentrationPm2p5, massConcentrationPm4p0, massConcentrationPm10p0,
-        ambientHumidity, ambientTemperature, vocIndex, noxIndex);
-    if (!sen5xError) {
-        // Print SEN5x measurements
-        Serial.println("SEN5x Measurements:");
-        Serial.print("MassConcentrationPm1p0:");
-        Serial.print(massConcentrationPm1p0);
-        Serial.print("\n");
-        Serial.print("MassConcentrationPm2p5:");
-        Serial.print(massConcentrationPm2p5);
-        Serial.print("\n");
-        Serial.print("MassConcentrationPm4p0:");
-        Serial.print(massConcentrationPm4p0);
-        Serial.print("\n");
-        Serial.print("MassConcentrationPm10p0:");
-        Serial.print(massConcentrationPm10p0);
-        Serial.print("\n");
-        Serial.print("AmbientHumidity:");
-        if (isnan(ambientHumidity)) {
-            Serial.print("n/a");
-        } else {
-            Serial.print(ambientHumidity);
-        }
-        Serial.print("\n");
-        Serial.print("AmbientTemperature:");
-        if (isnan(ambientTemperature)) {
-            Serial.print("n/a");
-        } else {
-            Serial.print(ambientTemperature);
-        }
-        Serial.print("\n");
-        Serial.print("VocIndex:");
-        if (isnan(vocIndex)) {
-            Serial.print("n/a");
-        } else {
-            Serial.print(vocIndex);
-        }
-        Serial.print("\n");
-        Serial.print("NoxIndex:");
-        if (isnan(noxIndex)) {
-            Serial.println("n/a");
-        } else {
-            Serial.println(noxIndex);
-        }
-    } else {
-        char errorMessage[256];
-        errorToString(sen5xError, errorMessage, sizeof(errorMessage));
-        Serial.print("SEN5x readMeasuredValues error: ");
-        Serial.println(errorMessage);
-    }
+      // Read and print SEN5x measurements
+      float massConcentrationPm1p0, massConcentrationPm2p5, massConcentrationPm4p0, massConcentrationPm10p0;
+      float ambientHumidity, ambientTemperature, vocIndex, noxIndex;
+      uint16_t sen5xError = sen5x.readMeasuredValues(
+          massConcentrationPm1p0, massConcentrationPm2p5, massConcentrationPm4p0, massConcentrationPm10p0,
+          ambientHumidity, ambientTemperature, vocIndex, noxIndex);
+      if (!sen5xError) {
+          // Print SEN5x measurements
+          Serial.println("SEN5x Measurements:");
+          Serial.print("MassConcentrationPm1p0:");
+          Serial.print(massConcentrationPm1p0);
+          Serial.print("\n");
+          Serial.print("MassConcentrationPm2p5:");
+          Serial.print(massConcentrationPm2p5);
+          Serial.print("\n");
+          Serial.print("MassConcentrationPm4p0:");
+          Serial.print(massConcentrationPm4p0);
+          Serial.print("\n");
+          Serial.print("MassConcentrationPm10p0:");
+          Serial.print(massConcentrationPm10p0);
+          Serial.print("\n");
+          Serial.print("AmbientHumidity:");
+          if (isnan(ambientHumidity)) {
+              Serial.print("n/a");
+          } else {
+              Serial.print(ambientHumidity);
+          }
+          Serial.print("\n");
+          Serial.print("AmbientTemperature:");
+          if (isnan(ambientTemperature)) {
+              Serial.print("n/a");
+          } else {
+              Serial.print(ambientTemperature);
+          }
+          Serial.print("\n");
+          Serial.print("VocIndex:");
+          if (isnan(vocIndex)) {
+              Serial.print("n/a");
+          } else {
+              Serial.print(vocIndex);
+          }
+          Serial.print("\n");
+          Serial.print("NoxIndex:");
+          if (isnan(noxIndex)) {
+              Serial.println("n/a");
+          } else {
+              Serial.println(noxIndex);
+          }
+      } else {
+          char errorMessage[256];
+          errorToString(sen5xError, errorMessage, sizeof(errorMessage));
+          Serial.print("SEN5x readMeasuredValues error: ");
+          Serial.println(errorMessage);
+      }
 
-    // Read and print SCD4x measurements
-    uint16_t co2;
-    float temperature, humidity;
-    bool isDataReady = false;
-    uint16_t scd4xError = scd4x.getDataReadyFlag(isDataReady);
-    if (!scd4xError && isDataReady) {
-        scd4xError = scd4x.readMeasurement(co2, temperature, humidity);
-        if (!scd4xError) {
-            // Print SCD4x measurements
-            Serial.println("SCD4x Measurements:");
-            Serial.print("Co2:");
-            Serial.print(co2);
-            Serial.print("\n");
-            Serial.print("Temperature:");
-            Serial.print(temperature);
-            Serial.print("\n");
-            Serial.print("Humidity:");
-            Serial.println(humidity);
-        } else {
-            char errorMessage[256];
-            errorToString(scd4xError, errorMessage, sizeof(errorMessage));
-            Serial.print("SCD4x readMeasurement error: ");
-            Serial.println(errorMessage);
-        }
-    }
+      // Read and print SCD4x measurements
+      uint16_t co2;
+      float temperature, humidity;
+      bool isDataReady = false;
+      uint16_t scd4xError = scd4x.getDataReadyFlag(isDataReady);
+      if (!scd4xError && isDataReady) {
+          scd4xError = scd4x.readMeasurement(co2, temperature, humidity);
+          if (!scd4xError) {
+              // Print SCD4x measurements
+              Serial.println("SCD4x Measurements:");
+              Serial.print("Co2:");
+              Serial.print(co2);
+              Serial.print("\n");
+              Serial.print("Temperature:");
+              Serial.print(temperature);
+              Serial.print("\n");
+              Serial.print("Humidity:");
+              Serial.println(humidity);
+          } else {
+              char errorMessage[256];
+              errorToString(scd4xError, errorMessage, sizeof(errorMessage));
+              Serial.print("SCD4x readMeasurement error: ");
+              Serial.println(errorMessage);
+          }
+      }
 
-    // Read and Print MQ4 Measurements
-    int methaneSensorValue = analogRead(mq4SensorPin);
-    Serial.print("Methane Gas Sensor Value: ");
-    Serial.println(methaneSensorValue);
+      // Read and Print MQ4 Measurements
+      int methaneSensorValue = analogRead(mq4SensorPin);
+      Serial.print("Methane Gas Sensor Value: ");
+      Serial.println(methaneSensorValue);
 
-    // Read and Print GPS Data
-    while (SerialGPS.available() > 0) {
-        char c = SerialGPS.read(); // Read a character from the GPS serial
-        if (gps.encode(c) && !gpsDataProcessed) { // If new GPS data is decoded and not yet processed
-            // It's important to keep decoding all available data to keep the GPS data stream fluent
-            if (gps.location.isValid()) {
-                // Print latitude and longitude once per loop after the delay
-                Serial.print("Latitude: ");
-                Serial.println(gps.location.lat(), 6); // 6 decimal places
-                Serial.print("Longitude: ");
-                Serial.println(gps.location.lng(), 6); // 6 decimal places
-            }
+      // Read and Print GPS Data
+      while (SerialGPS.available() > 0) {
+          char c = SerialGPS.read(); // Read a character from the GPS serial
+          if (gps.encode(c) && !gpsDataProcessed) { // If new GPS data is decoded and not yet processed
+              // It's important to keep decoding all available data to keep the GPS data stream fluent
+              if (gps.location.isValid()) {
+                  // Print latitude and longitude once per loop after the delay
+                  Serial.print("Latitude: ");
+                  Serial.println(gps.location.lat(), 6); // 6 decimal places
+                  Serial.print("Longitude: ");
+                  Serial.println(gps.location.lng(), 6); // 6 decimal places
+              }
 
-            if (gps.date.isValid()) {
-                // Print date
-                Serial.print("Date: ");
-                Serial.print(gps.date.day());
-                Serial.print("/");
-                Serial.print(gps.date.month());
-                Serial.print("/");
-                Serial.println(gps.date.year());
-            }
+              if (gps.date.isValid()) {
+                  // Print date
+                  Serial.print("Date: ");
+                  Serial.print(gps.date.day());
+                  Serial.print("/");
+                  Serial.print(gps.date.month());
+                  Serial.print("/");
+                  Serial.println(gps.date.year());
+              }
 
-            if (gps.time.isValid()) {
-                // Print time
-                Serial.print("Time: ");
-                Serial.print(gps.time.hour());
-                Serial.print(":");
-                Serial.print(gps.time.minute());
-                Serial.print(":");
-                Serial.println(gps.time.second());
-            }
+              if (gps.time.isValid()) {
+                  // Print time
+                  Serial.print("Time: ");
+                  Serial.print(gps.time.hour());
+                  Serial.print(":");
+                  Serial.print(gps.time.minute());
+                  Serial.print(":");
+                  Serial.println(gps.time.second());
+              }
 
-            // After printing, set flag to true to avoid printing repeatedly in this loop iteration
-            gpsDataProcessed = true;
-        }
-    }
+              // After printing, set flag to true to avoid printing repeatedly in this loop iteration
+              gpsDataProcessed = true;
+          }
+      }
 
-    // Write to SD Card
-    String dataLine = String(gps.date.year()) + "-" + String(gps.date.month()) + "-" + String(gps.date.day()) + "," +
-                  String(gps.time.hour()) + "." + String(gps.time.minute()) + "." + String(gps.time.second()) + "," +
-                  String(gps.location.lat(), 6) + "," + 
-                  String(gps.location.lng(), 6) + "," +
-                  String(massConcentrationPm1p0) + "," + 
-                  String(massConcentrationPm2p5) + "," + 
-                  String(massConcentrationPm4p0) + "," + 
-                  String(massConcentrationPm10p0) + "," + 
-                  String(ambientHumidity) + "," + 
-                  String(ambientTemperature) + "," + 
-                  String(vocIndex) + "," + 
-                  String(noxIndex) + "," + 
-                  String(co2) + "," + 
-                  String(temperature) + "," + 
-                  String(humidity) + "," + 
-                  String(methaneSensorValue) + "," + 
-                  String(xAccel, 3) + "," + 
-                  String(yAccel, 3) + "," + 
-                  String(zAccel, 3);
+      // Write to SD Card
+      String dataLine = String(gps.date.year()) + "-" + String(gps.date.month()) + "-" + String(gps.date.day()) + "," +
+                    String(gps.time.hour()) + "." + String(gps.time.minute()) + "." + String(gps.time.second()) + "," +
+                    String(gps.location.lat(), 6) + "," + 
+                    String(gps.location.lng(), 6) + "," +
+                    String(massConcentrationPm1p0) + "," + 
+                    String(massConcentrationPm2p5) + "," + 
+                    String(massConcentrationPm4p0) + "," + 
+                    String(massConcentrationPm10p0) + "," + 
+                    String(ambientHumidity) + "," + 
+                    String(ambientTemperature) + "," + 
+                    String(vocIndex) + "," + 
+                    String(noxIndex) + "," + 
+                    String(co2) + "," + 
+                    String(temperature) + "," + 
+                    String(humidity) + "," + 
+                    String(methaneSensorValue) + "," + 
+                    String(xAccel, 3) + "," + 
+                    String(yAccel, 3) + "," + 
+                    String(zAccel, 3);
 
-    logDataToSDCard(dataLine);
-
-    if (xAccel < 0)
-    {
-      delay(5000);
-    }
-    else
-    {
-      delay(30000);
-    }
+      logDataToSDCard(dataLine);
 }
